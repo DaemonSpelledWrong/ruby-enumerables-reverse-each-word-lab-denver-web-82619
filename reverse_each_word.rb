85d5d2d5bc91402fadf616cf6182b1b0913f6c81
Damon Self <damongself@gmail.com>
2019-08-16 12:47:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,4 @@
 def reverse_each_word(string)
   new_array = string.split(" ")
-  reversed_array = new_array.collect {|x| x.reverse!}
-  reversed_array.join(" ")
+  reversed_array = new_array.collect {|x| x.reverse!}.join(" ")
 end
