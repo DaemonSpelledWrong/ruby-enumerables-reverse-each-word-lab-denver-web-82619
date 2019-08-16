9fa1abad3229cd5660ee776d313d1a5b462ee42f
Damon Self <damongself@gmail.com>
2019-08-16 12:39:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   split_string = string.split("")
   reversed = []
-  string.size.times { reversed << split_string.unshift }
+  string.size.times { reversed << split_string. }
   reversed.join
 end
